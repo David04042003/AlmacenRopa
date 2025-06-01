4fe9fe5db0931f24e9c10e50c482c2dd2ef0adaa
stefanyceballo313470 <stefanyceballosescobar@gmail.com>
2025-06-01 13:59:37 -0500

    dashboard

---
+++ AlmacenRopa/Clases/clsDashboard.cs
@@ -0,0 +1,86 @@
+﻿using AlmacenRopa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlmacenRopa.Clases
+{
+    public class clsDashboard
+    {
+            private ALMACEN_ROPAEntities db = new ALMACEN_ROPAEntities();
+
+            // Totales
+            public int ObtenerTotalVentas()
+            {
+                return db.VENTAS.Count();
+            }
+
+            public int ObtenerTotalDevoluciones()
+            {
+                return db.DEVOLUCIONs.Count();
+            }
+
+            public int ObtenerTotalPedidos()
+            {
+                return db.PEDIDOes.Count();
+            }
+
+            // Ventas por día
+            public List<object> ObtenerVentasPorDia()
+            {
+                var resultado = db.VENTAS
+                    .GroupBy(v => v.FECHA.Date)
+                    .Select(g => new {
+                        Fecha = g.Key,
+                        TotalVentas = g.Count()
+                    }).ToList<object>();
+
+                return resultado;
+            }
+
+            // Productos más vendidos
+            public List<object> ObtenerProductosMasVendidos()
+            {
+                var resultado = db.DETALLEVENTAs
+                    .GroupBy(d => d.STOCK)
+                    .Select(g => new {
+                        Producto = g.FirstOrDefault().STOCK.PRODUCTO_VARIANTE,
+                        CantidadVendida = g.Sum(d => d.CANTIDAD)
+                    })
+                    .OrderByDescending(x => x.CantidadVendida)
+                    .Take(10)
+                    .ToList<object>();
+
+                return resultado;
+            }
+
+            // Alertas
+            public List<PEDIDO> ObtenerPedidosPendientes()
+            {
+                return db.PEDIDOes.Where(p => p.ESTADO == false).ToList();
+            }
+
+            public List<object> ObtenerProductosConStockBajo(int limite = 5)
+            {
+                return db.STOCKs
+                    .Where(s => s.CANTIDAD <= limite)
+                    .Select(s => new {
+                        Producto = s.PRODUCTO_VARIANTE.PRODUCTO.NOMBRE,
+                        Variante = s.PRODUCTO_VARIANTE.PRODUCTO,
+                        Stock = s.CANTIDAD
+                    }).ToList<object>();
+            }
+
+            // Resumen
+            public int ObtenerTotalClientes()
+            {
+                return db.CLIENTES.Count();
+            }
+
+            public int ObtenerTotalProductos()
+            {
+                return db.PRODUCTO_VARIANTE.Count();
+            }
+        }
+    }
---
+++ AlmacenRopa/Controllers/DashboardController.cs
@@ -0,0 +1,54 @@
+﻿using AlmacenRopa.Clases;
+using AlmacenRopa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AlmacenRopa.Controllers
+{
+
+        [RoutePrefix("api/dashboard")]
+        public class DashboardController : ApiController
+        {
+            clsDashboard dashboard = new clsDashboard();
+
+            [HttpGet]
+            [Route("TotalVentas")]
+            public int TotalVentas() => dashboard.ObtenerTotalVentas();
+
+            [HttpGet]
+            [Route("TotalDevoluciones")]
+            public int TotalDevoluciones() => dashboard.ObtenerTotalDevoluciones();
+
+            [HttpGet]
+            [Route("TotalPedidos")]
+            public int TotalPedidos() => dashboard.ObtenerTotalPedidos();
+
+            [HttpGet]
+            [Route("VentasPorDia")]
+            public List<object> VentasPorDia() => dashboard.ObtenerVentasPorDia();
+
+            [HttpGet]
+            [Route("ProductosMasVendidos")]
+            public List<object> ProductosMasVendidos() => dashboard.ObtenerProductosMasVendidos();
+
+            [HttpGet]
+            [Route("PedidosPendientes")]
+            public List<PEDIDO> PedidosPendientes() => dashboard.ObtenerPedidosPendientes();
+
+            [HttpGet]
+            [Route("ProductosConStockBajo")]
+            public List<object> ProductosConStockBajo() => dashboard.ObtenerProductosConStockBajo();
+
+            [HttpGet]
+            [Route("TotalClientes")]
+            public int TotalClientes() => dashboard.ObtenerTotalClientes();
+
+            [HttpGet]
+            [Route("TotalProductos")]
+            public int TotalProductos() => dashboard.ObtenerTotalProductos();
+        }
+    }
